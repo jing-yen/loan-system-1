1b1eef66d59518ebf4458b215a8f24f5fd1c2f87
Jing Yen <tong414782@gmail.com>
2025-04-23 11:42:45 +0800

    Port App.js to TS

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
     setVerifying(true);
   }
 
-  const handleVerificationResponse = (verified) => {
+  const handleVerificationResponse = (verified: boolean) => {
     console.log('Verification response:', verified);
     setVerifiedByStaff(verified);
     setVerifying(false); 
